67ed0d3ca31a169f8c0ef1e536d9c2b73443c87c
nawshikha <nawshikhamahato@gmail.com>
2024-07-01 14:04:16 +0530

    gitignore

--- server/database/db.js
+++ server/database/db.js
@@ -7,8 +7,10 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD; 
 
 const Connection = () => {
-    const DB_URI = `mongodb://127.0.0.1:27017/gmail`;
+    const DB_URI = `mongodb+srv://nawshikha:R7He7gfkyORfIaHF@cluster0.d3xumja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     try {
+        mongoose.set('debug', true);
+
         mongoose.connect(DB_URI, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
         console.log('Database connected sucessfully');
